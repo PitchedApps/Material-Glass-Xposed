2e6a7e61a54871256d2139c08f26e267cb6f31ca
AllanWang <me@allanwang.ca>
2016-11-06 01:52:19 -0500

    big rewrite

--- XMaterialGlass/src/main/java/com/pitchedapps/material/glass/xposed/ThemePreferences.java
+++ XMaterialGlass/src/main/java/com/pitchedapps/material/glass/xposed/ThemePreferences.java
@@ -3,34 +3,32 @@ package com.pitchedapps.material.glass.xposed;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
+import android.support.annotation.StringRes;
 
 import com.pitchedapps.material.glass.xposed.utilities.Common;
 import com.pitchedapps.material.glass.xposed.utilities.Packages;
 import com.pitchedapps.material.glass.xposed.utilities.Preferences;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 /**
- * Created by 7681 on 2016-02-18.
+ * Created by Allan Wang on 2016-02-18.
  */
 
 public class ThemePreferences extends PreferenceFragment {
 
     private Context mContext;
     private Preferences prefs;
+    private PreferenceScreen prefScreen;
 
     @SuppressWarnings("deprecation")
     @Override
@@ -40,21 +38,25 @@ public class ThemePreferences extends PreferenceFragment {
         mContext = getActivity();
         prefs = new Preferences(mContext);
         getPreferenceManager().setSharedPreferencesMode(Context.MODE_WORLD_READABLE);
-        PreferenceScreen prefScreen = getPreferenceManager().createPreferenceScreen(mContext);
+        prefScreen = getPreferenceManager().createPreferenceScreen(mContext);
         setPreferenceScreen(prefScreen);
-        
+
         prefs.setVersionCode(BuildConfig.VERSION_CODE);
 
-        PreferenceCategory cMain = new PreferenceCategory(mContext);
-        cMain.setTitle(R.string.pref_category_main);
-        prefScreen.addPreference(cMain);
-
-        initPreferences(cMain);
+        initPreferences(R.string.pref_category_main, Packages.values());
     }
 
-    private void initPreferences(PreferenceCategory category) {
+    private void initPreferences(@StringRes int titleId, Packages... pp) {
+        initPreferences(getString(titleId), pp);
+    }
 
-        List<Packages> packages = Arrays.asList(Packages.values());
+    private void initPreferences(String title, Packages... pp) {
+
+        PreferenceCategory category = new PreferenceCategory(mContext);
+        category.setTitle(title);
+        prefScreen.addPreference(category);
+
+        List<Packages> packages = Arrays.asList(pp);
         Collections.sort(packages, new Comparator<Packages>() {
             @Override
             public int compare(Packages p1, Packages p2) {
@@ -68,6 +70,7 @@ public class ThemePreferences extends PreferenceFragment {
             for (String s : p.getDescription()) {
                 desc += "\n\u2022 " + s;
             }
+            if (desc.length() > 1 && desc.charAt(0) == '\n') desc = desc.substring(1);
             final String message = desc;
             pr.setKey(p.getPrefKey()); //app name
             pr.setTitle(p.toString());
